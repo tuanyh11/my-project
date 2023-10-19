c673796db3d51e6e7ac1f1914a88b81bb858b2fb
tuan <tuan>
2023-10-19 14:11:59 +0700

    change

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,11 @@ const cors = require("cors");
 
 dbConnect();
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
