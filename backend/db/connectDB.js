6ee4ba83c57a04b1e51e9c29d92a7c9a3be25db4
Tuấn Leo <vantuanxyz741@gmail.com>
2024-08-22 01:16:47 +0700

    sec commit

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,18 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			// To avoid warnings in the console
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+  console.log(process.env.MONGO_DB_URI);
 
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-		process.exit(1);
-	}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI, {
+      // To avoid warnings in the console
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,50 +1,54 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		username: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			minLength: 6,
-			required: true,
-		},
-		profilePic: {
-			type: String,
-			default: "",
-		},
-		followers: {
-			type: [String],
-			default: [],
-		},
-		following: {
-			type: [String],
-			default: [],
-		},
-		bio: {
-			type: String,
-			default: "",
-		},
-		isFrozen: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	{
-		timestamps: true,
-	}
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    // phoneNumber: {
+    //   type: Number,
+    //   required: true,
+    // },
+    password: {
+      type: String,
+      minLength: 6,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: [String],
+      default: [],
+    },
+    following: {
+      type: [String],
+      default: [],
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    isFrozen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const User = mongoose.model("User", userSchema);
